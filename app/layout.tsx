7ba67196397b84558ab163917b130c5ac8c11ed2
marleymcbride <business.marleymcbride@gmail.com>
2025-12-13 19:23:21 +0000

    Remove broken SemiBold font file and update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ const inter = localFont({
   src: [
     { path: './fonts/Inter-Regular.woff2', weight: '400' },
     { path: './fonts/Inter-Medium.woff2', weight: '500' },
-    { path: './fonts/Inter-SemiBold.woff2', weight: '600' },
     { path: './fonts/Inter-Bold.woff2', weight: '700' }
   ],
   variable: '--font-inter',
